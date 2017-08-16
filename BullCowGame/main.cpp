a4e2061436539790831c4901c345c069f5999c42
erinlow <erin.low95@gmail.com>
2017-08-16 09:59:44 -0700

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Erin Low on 8/16/17.
+//  Copyright © 2017 Erin Low. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
